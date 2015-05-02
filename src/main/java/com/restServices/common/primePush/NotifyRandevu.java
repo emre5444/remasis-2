4ef687a3cba0b2a1439df06adb6b92fe243c4ab9
fatihcabi <fatihcabi@8c96c0c0-8daa-e585-a0db-5c7ea11d28f7>
2015-05-02 02:13:22 +0000

    prime push ile randevu real time olacak şekilde guncellendi.

---
+++ src/main/java/com/restServices/common/primePush/NotifyRandevu.java
@@ -0,0 +1,11 @@
+package com.restServices.common.primePush;
+
+import org.primefaces.push.annotation.PushEndpoint;
+
+/**
+ * Created by fcabi on 02.05.2015.
+ */
+
+@PushEndpoint("/notifyRandevu")
+public class NotifyRandevu {
+}
